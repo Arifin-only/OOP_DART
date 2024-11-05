16903df8681b4746fe89ba7888f1f101eea97b8c
Arifin-only <arifin180104@gmail.com>
2024-11-05 11:58:59 +0800

    Error.dart

---
+++ Error.dart
@@ -0,0 +1,15 @@
+// //Error
+void main() {
+  var list = ["Muhammad", "Arifin", "Ilham"];
+  print(list[10]);
+}
+
+
+//Ini Tidak Error
+// void main() {
+//   var list = ["Muhammad", "Arifin", "Ilham"];
+
+//   if (list.length > 10) {
+//     print(list[10]);
+//   }
+// }
